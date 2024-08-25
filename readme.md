e58217f7671c62e9d1e0f96159931c82c9361d5a
Patrick Wilson <patrick@pwry.net>
2024-08-25 18:45:31 -0500

    Some much-needed updates.

--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
 # CampingGame
-A simple game idea that was thought of by people. You play a game deciding who is going to go camping with you. The initial game rule was to choose an item that you are going to take, using the first name of the person playing.
+A simple game idea that was thought of by people at my school. You play a game deciding who is going to go camping with you. The initial game rule was to choose an item that you are going to take, using the first name of the person playing.
 
-This is an attempt to replicate this game.
+This is an attempt to replicate this game. It was written in Java because that was the programming language I was learning back then in my Computer Science class.
 
-# Contributing.
-If you have any more rule ideas or suggestions, do feel free to submit them as a PR or an issue.
+## Contributing.
+If you have any more rule ideas or suggestions, do feel free to submit them as a PR or an issue!
 
--- src/CampingGame.java
+++ src/CampingGame.java
@@ -5,23 +5,23 @@
 import java.util.Scanner;
 
 public class CampingGame {
-public static void main(String[] args) {
-String firstletter=ask("Suppose that this is the first letter of your name, or some other first letter. Please enter it here so that you can quiz the users to see if they can come or not.");
-String current="";
-while(!current.contains("quit")) {
-current=ask("Enter a guess.");
-if(!current.contains("quit")) {
-if(!current.startsWith(firstletter)) System.out.println("Uh-oh, you can't go to the camp!");
-else System.out.println("You can go to the camp with us.");
-} //If
-} //While
-System.out.println("Thank you for playing the game!");
-} //Main
-public static String ask(String what) {
-Scanner key=new Scanner(System.in);
-System.out.println(what);
-String tempText=key.nextLine();
-return tempText;
-} //Ask
-
-} //Class
+	public static void main(String[] args) {
+		String firstletter = ask("Suppose that this is the first letter of your name, or some other first letter. Please enter it here so that you can quiz the users to see if they can come or not.");
+		String current = "";
+		while (!current.contains("quit")) {
+			current = ask("Enter a guess.");
+			if (!current.contains("quit")) {
+				if (!current.startsWith(firstletter)) System.out.println("Uh-oh, you can't go to the camp!");
+				else System.out.println("You can go to the camp with us.");
+			}
+		}
+		System.out.println("Thank you for playing the game!");
+	}
+	
+	public static String ask(String what) {
+		Scanner key = new Scanner(System.in);
+		System.out.println(what);
+		String tempText = key.nextLine();
+		return tempText;
+	}
+}
